4114541881211ffd17d0ba8d3cb0bf441a8d3f62
smaasath <99379342+smaasath@users.noreply.github.com>
2023-07-20 21:27:50 +0530

    table template

--- index.php
+++ index.php
@@ -11,8 +11,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         <?php
        echo 'ss';
-       hhh
-       jnn
+      
         ?>
     </body>
 </html>
