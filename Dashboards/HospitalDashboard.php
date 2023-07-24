80e1002549d665e4223ec438c4d0eeee02735ae6
Saalu <saalu2023@gmail.com>
2023-07-24 23:35:40 +0530

    Hospital blood stock,Alert

--- Dashboards/HospitalDashboard.php
+++ Dashboards/HospitalDashboard.php
@@ -34,6 +34,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="../CSS/Stock.css">
     </head>
     <body>
         <div class="container-fluid">
