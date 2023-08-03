dd297244ad17906bd174024b76d5a9b5228d6fda
Sankavi <sankaviraveendran@gmail.com>
2023-08-03 21:55:31 +0530

    footer css file modified. admin profile modified.

--- DashboardFiles/AdminFiles/AdminProfile.php
+++ DashboardFiles/AdminFiles/AdminProfile.php
@@ -13,17 +13,12 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <style>
             body {
             font-family: Arial, Helvetica, sans-serif;
-            align: center;
-            display: flex;
-            felx:50%;
-            
             }
             
             form {
             border: 3px solid #f1f1f1;
-            width: 50%;
-            padding-left:25px;
-            padding-right:25px;
+            border-radius: 30px;
+            width: 50%;            
             position: relative;
             left: 25%;
             
@@ -35,6 +30,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
               margin: 8px 0;
               display: inline-block;
               border: 1px solid #ccc;
+              border-radius: 10px;
               box-sizing: border-box;
             }
 
@@ -44,6 +40,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
               padding: 14px 20px;
               margin: 8px 0;
               border: none;
+              border-radius: 10px;
               cursor: pointer;
               width: 100%;
             }
@@ -125,15 +122,17 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
             <label for="uname"><b>Username</b></label><br>
             <input type="text" placeholder="Enter Username" name="uname" required><br>
 
-            <label for="psw"><b>Password</b></label><br>
-            <input type="password" placeholder="Enter Password" name="psw" required><br>
+           <label for="psw"><b>Password</b></label><br>
+           <input type="password" placeholder="Enter Password" name="cpsw" id="typepass" required><br>
+            
 
-            <label for="cpsw"><b>Change Password</b></label><br>
-            <input type="password" placeholder="change your password" name="cpsw" required><br>
 
-            <label>
-              <input type="checkbox" checked="checked" name="remember">Show password
-            </label>
+            <label for="cpsw"><b> Change Password</b></label><br>
+            <input type="password" placeholder="Enter Password" name="cpsw" id="typepass" required><br>
+
+            
+             <input type="checkbox" onclick="Toggle()">
+             <label>Show password</label>
 
             <button type="submit">Login</button>
 
@@ -143,184 +142,25 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
           </div>
         </form>
 
-
-
-        <!-- Table -->
-
-        <div class="p-5">
-
-
-            <div class="rounded-top-4 p-0 border border-dark-subtle">
-                <div class="row align-items-center">
-                    <div class="col-3">           
-                        <div class="input-group rounded p-3">
-                            <input type="search" class="form-control rounded" placeholder="Search ID" aria-label="Search" aria-describedby="search-addon" >
-
-
-
-                        </div>
-                    </div>
-                    <div class="col-3"> 
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Department</option>
-                            <option value="1">Department1</option>
-                            <option value="2">Department2</option>
-                            <option value="3">Department3</option>
-                        </select>
-                    </div>
-
-                    <div class="col-2"> 
-                        <select class="form-select" aria-label="Default select example">
-                            <option selected>Course</option>
-                            <option value="1">Course 1</option>
-                            <option value="2">Course 2</option>
-                            <option value="3">Course 3</option>
-                        </select>
-                    </div>
-                    <div class="col-2"> 
-                        <button type="button" class="btn btn-primary bgcol" onclick="Addstudent()">Add Student</button>
-                    </div>
-
-                    <div class="col-2" >
-                        <button type="button" class="btn btn-success">Save</button>
-                    </div>
-                </div>
-
-
-
-
-
-
-
-
-
-            </div>
-            <!-- Table body -->
-            <div class="container bg-white m-0 p-0" style=" max-height: 373px; overflow: scroll;">
-                <table class="table table-hover p-0">
-
-                    <!-- Table row -->
-
-
-                    <tr class="sticky-top">
-
-                        <th class="col-1 bgcol p-2">ID</th>
-                        <th class="col-3 bgcol p-2">Name</th>
-                        <th class="col-2 bgcol p-2">Gender</th>
-                        <th class="col-3 bgcol p-2">Contact No</th>
-                        <th class="col-1 bgcol p-2">View</th>
-                        <th class="col-1 bgcol p-2">Edit</th>
-
-                    </tr>
-
-                    <tr>
-                        <td class="col-1">0001</td>
-                        <td class="col-3">Mohamed Aasath</td>
-                        <td class="col-2">Male</td>
-                        <td class="col-3">0755701765</td>
-                        <td class="col-1"><button type="button" class="btn btn-info" onclick="openStudentDetails()">View</button></td>
-                        <td class="col-1"><button type="button" class="btn btn-secondary" onclick="EditStudent()">Edit</button></td>
-                    </tr>
-
-                    <tr>
-                        <td class="col-1">0001</td>
-                        <td class="col-3">Mohamed Aasath</td>
-                        <td class="col-2">Male</td>
-                        <td class="col-3">0755701765</td>
-                        <td class="col-1"><button type="button" class="btn btn-info" onclick="openStudentDetails()">View</button></td>
-                        <td class="col-1"><button type="button" class="btn btn-secondary" onclick="EditStudent()">Edit</button></td>
-                    </tr>
-
-                    <tr>
-                        <td class="col-1">0001</td>
-                        <td class="col-3">Mohamed Aasath</td>
-                        <td class="col-2">Male</td>
-                        <td class="col-3">0755701765</td>
-                        <td class="col-1"><button type="button" class="btn btn-info" onclick="openStudentDetails()">View</button></td>
-                        <td class="col-1"><button type="button" class="btn btn-secondary" onclick="EditStudent()">Edit</button></td>
-                    </tr>
-
-                    <tr>
-                        <td class="col-1">0001</td>
-                        <td class="col-3">Mohamed Aasath</td>
-                        <td class="col-2">Male</td>
-                        <td class="col-3">0755701765</td>
-                        <td class="col-1"><button type="button" class="btn btn-info" onclick="openStudentDetails()">View</button></td>
-                        <td class="col-1"><button type="button" class="btn btn-secondary" onclick="EditStudent()">Edit</button></td>
-                    </tr>
-
-                    <tr>
-                        <td class="col-1">0001</td>
-                        <td class="col-3">Mohamed Aasath</td>
-                        <td class="col-2">Male</td>
-                        <td class="col-3">0755701765</td>
-                        <td class="col-1"><button type="button" class="btn btn-info" onclick="openStudentDetails()">View</button></td>
-                        <td class="col-1"><button type="button" class="btn btn-secondary" onclick="EditStudent()">Edit</button></td>
-                    </tr>
-
-                    <tr>
-                        <td class="col-1">0001</td>
-                        <td class="col-3">Mohamed Aasath</td>
-                        <td class="col-2">Male</td>
-                        <td class="col-3">0755701765</td>
-                        <td class="col-1"><button type="button" class="btn btn-info" onclick="openStudentDetails()">View</button></td>
-                        <td class="col-1"><button type="button" class="btn btn-secondary" onclick="EditStudent()">Edit</button></td>
-                    </tr>
-
-                    <tr>
-                        <td class="col-1">0001</td>
-                        <td class="col-3">Mohamed Aasath</td>
-                        <td class="col-2">Male</td>
-                        <td class="col-3">0755701765</td>
-                        <td class="col-1"><button type="button" class="btn btn-info" onclick="openStudentDetails()">View</button></td>
-                        <td class="col-1"><button type="button" class="btn btn-secondary" onclick="EditStudent()">Edit</button></td>
-                    </tr>
-
-                    <tr>
-                        <td class="col-1">0001</td>
-                        <td class="col-3">Mohamed Aasath</td>
-                        <td class="col-2">Male</td>
-                        <td class="col-3">0755701765</td>
-                        <td class="col-1"><button type="button" class="btn btn-info" onclick="openStudentDetails()">View</button></td>
-                        <td class="col-1"><button type="button" class="btn btn-secondary" onclick="EditStudent()">Edit</button></td>
-                    </tr>
-
-                    <tr>
-                        <td class="col-1">0001</td>
-                        <td class="col-3">Mohamed Aasath</td>
-                        <td class="col-2">Male</td>
-                        <td class="col-3">0755701765</td>
-                        <td class="col-1"><button type="button" class="btn btn-info" onclick="openStudentDetails()">View</button></td>
-                        <td class="col-1"><button type="button" class="btn btn-secondary" onclick="EditStudent()">Edit</button></td>
-                    </tr>
-
-
-                    <!-- Table row -->
-
-                </table> 
-
-            </div>
-            <br>
-            <!-- Table Head -->
-          
-        </div>
-
-        <!-- Table -->
-
-
-
-
-        
-
-
-
         <?php
         // put your code here
         ?>
 
+        <script>
+        // Change the type of input to password or text
+        function Toggle() {
+            let temp = document.getElementById("typepass");
+             
+            if (temp.type === "password") {
+                temp.type = "text";
+            }
+            else {
+                temp.type = "password";
+            }
+        }
+        </script>
 
-
-
+         
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
