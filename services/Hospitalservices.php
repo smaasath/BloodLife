61263dadf3870b0fcd93008eea1c770a4cbb9bc2
Sankavi-Raveendran <sankaviraveendran@gmail.com>
2023-11-22 21:46:46 +0530

    homepages

--- services/Hospitalservices.php
+++ services/Hospitalservices.php
@@ -64,8 +64,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // echo $districtId;
     
    
-   
-   
     //create user object with token
    $user = new User(null, null, null, null, $token, null, null, null, null);
     
