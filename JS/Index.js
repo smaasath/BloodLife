1e55dbab9f3beb506011bef4e62bfe643af682a1
Aasath <aasadh2000@gmail.com>
2023-11-26 21:15:46 +0530

    solrathukku ondum illa....

---
+++ JS/Index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const PageSize = document.getElementById("pageSize");
+    const PageHeight = window.innerHeight;
+    const PageWidth = window.innerWidth;
+
+    PageSize.style.height = `${PageHeight}px`;
+    
+});
